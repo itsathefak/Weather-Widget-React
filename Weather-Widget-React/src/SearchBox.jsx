6ff65b843052990eede0723d0b865e031ed73e4b
itsathefak <athefayubkhan@gmail.com>
2024-12-28 01:41:33 -0500

    Use Weather Widget

--- Weather-Widget-React/src/SearchBox.jsx
+++ Weather-Widget-React/src/SearchBox.jsx
@@ -6,18 +6,39 @@ import { useState } from 'react';
 export default function SearchBox(){
     let [city, setCity] = useState("")
 
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather"
+  const API_KEY = "81b62895a293cfcd99b6874c16219950"
+       
+
+    let getWeatherInfo = async ()=>{
+       let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+       let jsonResponse = await response.json();
+       console.log(jsonResponse);
+       let result = {
+        temp : jsonResponse.main.temp,
+        tempMin : jsonResponse.main.temp_min,
+        tempMax : jsonResponse.main.temp_max,
+        humidity : jsonResponse.main.humidity,
+        feelsLike : jsonResponse.main.feels_like,
+        weather : jsonResponse.weather[0].description,
+       }
+       console.log(result)
+    }
+
     let handleChange = (evt)=>{
         setCity(evt.target.value);
     }
+
     let handleSubmit = (evt)=>{
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
     };
     return(
     <div className='SearchBox '>
         <h3>Search for the Weather</h3>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} >
         <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange} />
         <br /><br />
         <Button variant="contained" type='submit' >
